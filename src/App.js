4e98be2ac33c63a0fd2c55acc095614a10eb952a
yoonbyungyeon <yby104@naver.com>
2022-02-03 16:06:40 +0900

    Header

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
+import { Route, Routes } from "react-router-dom";
+import Header from "./Components/Header/Header";
 import Home from "./page/Home";
 
 function App() {
   return (
     <div>
+      <Header/>
       <Routes>
         <Route path="/" element={<Home />} />
       </Routes>
---
+++ src/Components/Header/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Header = () => {
+    return (<div>
+        <Link to="/">홈</Link>
+        <Link to="page1">페이지1</Link>
+        <Link to="page2">페이지2</Link>
+    </div>);
+}
+ 
+export default Header
