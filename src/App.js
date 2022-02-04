c5164274e4269dba7d55e383757870b02b9acc7f
yoonbyungyeon <yby104@naver.com>
2022-02-04 15:41:47 +0900

    styled components

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Header from "./Components/Header/Header";
 import Home from "./page/Home";
 import Page1 from "./page/Page1";
 import Page2 from "./page/Page2";
+import Frame from "./page/Frame";
 
 function App() {
 
